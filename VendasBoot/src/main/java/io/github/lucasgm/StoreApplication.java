14d70a5290e7e6ea2de6976e8e66cee36ea1a010
lucas <lucas.goedert@ndd.tech>
2022-06-10 00:09:18 -0300

    Adicionando ProductController

--- VendasBoot/src/main/java/io/github/lucasgm/StoreApplication.java
+++ VendasBoot/src/main/java/io/github/lucasgm/StoreApplication.java
@@ -1,10 +1,23 @@
 package io.github.lucasgm;
 
+import io.github.lucasgm.domain.entity.Client;
+import io.github.lucasgm.domain.repository.IClientsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StoreApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired IClientsRepository clientsRepository){
+        return args -> {
+            Client client = new Client("Fulano");
+            clientsRepository.save(client);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
---
+++ VendasBoot/src/main/java/io/github/lucasgm/api/controller/ProductController.java
@@ -0,0 +1,73 @@
+package io.github.lucasgm.api.controller;
+
+import io.github.lucasgm.domain.entity.Product;
+import io.github.lucasgm.domain.repository.IProductsRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private IProductsRepository repository;
+
+    public ProductController(IProductsRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product save(@RequestBody Product product) {
+        return repository.save(product);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Product product) {
+        repository
+                .findById(id)
+                .map(p -> {
+                    product.setId(p.getId());
+                    repository.save(product);
+                    return product;
+                }).orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado"));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        repository
+                .findById(id)
+                .map(p -> {
+                    repository.deleteById(id);
+                    return Void.TYPE;
+                }).orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado"));
+    }
+
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Product getById(@PathVariable Integer id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado"));
+    }
+
+    @GetMapping
+    public List<Product> find(Product filter) {
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filter, matcher);
+        return repository.findAll(example);
+    }
+}
